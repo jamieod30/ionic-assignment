d1f134b09a70cc646ba23edc85971336fe4fb1cc
unknown <jamieod30@gmail.com>
2015-05-02 15:14:47 +0100

    added basic navigation

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,14 @@
-angular.module('calorific', ['ionic'])
+angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    if (window.StatusBar) {
+      StatusBar.styleLightContent();
     }
   });
 })
@@ -17,16 +17,45 @@ angular.module('calorific', ['ionic'])
 
 	$stateProvider
 	
-		.state('home', {
-		  url: "/home",
-		  templateUrl: "index.html"
-		})
-		
-		.state('app', {
-		  url: "/app",
-		  templateUrl: "app.html"
-		});
-		
-		// if none of the above states are matched use this as the fallback
-	$urlRouterProvider.otherwise('/home');
+    .state('tab', {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.home', {
+    url: '/home',
+    views: {
+      'tab-home': {
+        templateUrl: 'templates/tab-home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('tab.foodlist', {
+      url: '/foodlist',
+      views: {
+        'tab-foodlist': {
+          templateUrl: 'templates/tab-foodlist.html',
+          controller: 'FoodlistCtrl'
+        }
+      }
+    })
+
+  .state('tab.calculatormenu', {
+    url: '/calculatormenu',
+    views: {
+      'tab-calculatormenu': {
+        templateUrl: 'templates/tab-calculatormenu.html',
+        controller: 'CalculatormenuCtrl'
+      }
+    }
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/home');
+
 });
