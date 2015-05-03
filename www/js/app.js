88ea5f83da56fea08a98b3b621ee37077d2ca46d
unknown <jamieod30@gmail.com>
2015-05-03 10:19:08 +0100

    added expanded navigation

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,10 @@
+// Ionic Calorie App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'Calorie' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'Calorie.services' is found in services.js
+// 'Calorie.controllers' is found in controllers.js
 angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
 
 .run(function($ionicPlatform) {
@@ -8,6 +15,7 @@ angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
   });
@@ -15,8 +23,13 @@ angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-	$stateProvider
-	
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
     .state('tab', {
     url: "/tab",
     abstract: true,
@@ -44,6 +57,16 @@ angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
         }
       }
     })
+	
+    .state('tab.foodlist-detail', {
+      url: '/foodlist/:foodlistId',
+      views: {
+        'tab-foodlist': {
+          templateUrl: 'templates/foodlist-detail.html',
+          controller: 'FoodListDetailCtrl'
+        }
+      }
+    })
 
   .state('tab.calculatormenu', {
     url: '/calculatormenu',
@@ -53,6 +76,21 @@ angular.module('Calorie', ['ionic', 'Calorie.controllers', 'Calorie.services'])
         controller: 'CalculatormenuCtrl'
       }
     }
+  })
+  
+    .state('calculator', {
+    url: '/calculator',
+    templateUrl: 'templates/calculator.html'
+  })
+  
+    .state('bmi', {
+    url: '/bmi',
+    templateUrl: 'templates/bmi.html'
+  })
+  
+    .state('counter', {
+    url: '/counter',
+    templateUrl: 'templates/counter.html'
   });
 
   // if none of the above states are matched, use this as the fallback
